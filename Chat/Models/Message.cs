af3162a860b80b22b4428845bb9fabcd47657c7b
Андрей Вартанян <a.vartanyan@rusrobots.ru>
2020-09-12 09:00:33 +0300

    Добавьте файлы проекта.

---
+++ Chat/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleChat.Chat.Models
+{
+    class Message
+    {
+        private string message;
+        private DateTime dateCreated;
+
+        public Message(string message)
+        {
+            this.message = message;
+            this.dateCreated = DateTime.Now;
+        }
+
+        public string GetMessage() { return this.message; }
+        public DateTime GetDateMessage() { return this.dateCreated; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+using System;
+namespace ConsoleChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ServerChat().start();
+        }
+    }
+}
